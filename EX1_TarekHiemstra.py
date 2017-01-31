ed61f87e0b73d4b91cb715a67d7a3121cd3bb674
Tarek Hiemstra <tarek.hiemstra@gmail.com>
2017-01-31 10:56:48 +0200

    Add exercise 1

---
+++ EX1_TarekHiemstra.py
@@ -0,0 +1,8 @@
+"""
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import platform
+>>> platform
+<module 'platform' from 'C:\\Program Files (x86)\\Python36-32\\lib\\platform.py'>
+>>> 
+"""
