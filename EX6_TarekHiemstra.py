d50c6afd6f973d400397dbf6e220fe8c373dbeaf
Tarek Hiemstra <tarek.hiemstra@gmail.com>
2017-02-02 15:54:49 +0200

    Improve readability and add a comment line to exercise 6

--- EX6_TarekHiemstra.py
+++ EX6_TarekHiemstra.py
@@ -14,7 +14,8 @@ def assignment_6(input1, input2):
     if extension != ".py":
         return -1
 
-# by using 'with' the files automatically close after dedenting
+    # by using 'with' the files automatically close after dedenting
+    # variable i because some comments are not at beginning of line
     with open(input1, "r") as file1, open(input2, "w") as file2:
         for lines in file1:
             comment = lines.strip()
@@ -22,12 +23,11 @@ def assignment_6(input1, input2):
             if match:
                 i = comment.index("#")
                 file2.write((comment[i:]).rstrip() + "\n")
-                
 
-# counting number of comments
+    # counting number of comments
     with open(input2, 'r') as file2:
         count = 0
         for lines in file2:
             count += 1
     return count
-#print(assignment_6("pythonfile1.py", "pythonfile2.py"))
+print(assignment_6("pythonfile1.py", "pythonfile2.py"))
