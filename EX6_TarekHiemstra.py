6393e475b9222204ef96edddba915f86206d6289
Tarek Hiemstra <tarek.hiemstra@gmail.com>
2017-02-02 12:03:08 +0200

    Add re module, add more comments and improve the readability of exercise 6

--- EX6_TarekHiemstra.py
+++ EX6_TarekHiemstra.py
@@ -2,17 +2,27 @@
 # Metropolia University of Applied Sciences
 # Assignment 6: Files
 # 03 February 2017
+
+
 import os
+import re
+
+
 def assignment_6(input1, input2):
+    # check for correct filetype
     extension = os.path.splitext(input1)[-1].lower()
     if extension != '.py':
         return -1
+
 # by using 'with' the files automatically close after dedenting
     with open(input1, 'r') as file1, open(input2, 'w') as file2:
         for lines in file1:
             comments = lines.strip()
-            if comments.startswith('#'):
+            match = re.search("#", comments)
+            if match:
                 file2.write(comments.rstrip() + '\n')
+
+# counting number of comments
     with open(input2, 'r') as file2:
         count = 0
         for lines in file2:
