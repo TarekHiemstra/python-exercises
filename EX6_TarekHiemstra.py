de05455b6d14c3950f43f97460d3806526d5542a
Tarek Hiemstra <tarek.hiemstra@gmail.com>
2017-02-03 11:57:22 +0200

    Change the filetype check by removing os module and place the counter in one and the same 'with block'

--- EX6_TarekHiemstra.py
+++ EX6_TarekHiemstra.py
@@ -1,21 +1,20 @@
 # Tarek Hiemstra
 # Metropolia University of Applied Sciences
 # Assignment 6: Files
-# 03 February 2017
+# 04 February 2017
 
 
-import os
 import re
 
 
 def assignment_6(input1, input2):
     # check for correct filetype
-    extension = os.path.splitext(input1)[-1].lower()
-    if extension != ".py":
+    if input1[-3:].lower() != ".py":
         return -1
 
     # by using 'with' the files automatically close after dedenting
     # variable i because some comments are not at beginning of line
+    count = 0
     with open(input1, "r") as file1, open(input2, "w") as file2:
         for lines in file1:
             comment = lines.strip()
@@ -23,11 +22,6 @@ def assignment_6(input1, input2):
             if match:
                 i = comment.index("#")
                 file2.write((comment[i:]).rstrip() + "\n")
-
-    # counting number of comments
-    with open(input2, 'r') as file2:
-        count = 0
-        for lines in file2:
-            count += 1
+                count += 1
     return count
-print(assignment_6("pythonfile1.py", "pythonfile2.py"))
+#print(assignment_6("pythonfile1.py", "pythonfile2.py"))
