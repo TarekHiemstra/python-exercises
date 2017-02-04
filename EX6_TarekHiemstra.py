e1e8f92163413c5508fd7637d1c6e0ad586a0a60
Tarek Hiemstra <tarek.hiemstra@gmail.com>
2017-02-04 15:44:00 +0200

    Add documentation to exercise 6. Final version.

--- EX6_TarekHiemstra.py
+++ EX6_TarekHiemstra.py
@@ -1,19 +1,20 @@
-# Tarek Hiemstra
-# Metropolia University of Applied Sciences
-# Assignment 6: Files
-# 04 February 2017
-
-
 import re
 
 
 def assignment_6(input1, input2):
-    # check for correct filetype
+    """
+    Tarek Hiemstra
+    Metropolia University of Applied Sciences
+    Assignment 6: Files
+    04 February 2017
+    """
+
+    # Check for correct filetype.
     if input1[-3:].lower() != ".py":
         return -1
 
-    # by using 'with' the files automatically close after dedenting
-    # variable i because some comments are not at beginning of line
+    # By using 'with' the files automatically close after dedenting.
+    # Variable i, because some comments are not at beginning of line.
     count = 0
     with open(input1, "r") as file1, open(input2, "w") as file2:
         for lines in file1:
@@ -24,4 +25,3 @@ def assignment_6(input1, input2):
                 file2.write((comment[i:]).rstrip() + "\n")
                 count += 1
     return count
-#print(assignment_6("pythonfile1.py", "pythonfile2.py"))
